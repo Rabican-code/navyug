9fcc0641a0891fde7a1fea1890d62b612b0a91d1
Rabican-code <rabikantsingh24@gmail.com>
2022-12-23 13:44:24 +0530

    first commit

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+
+Route::get('/admission', function () {
+    return view('admission');
+});
+
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+
+Route::get('/why', function () {
+    return view('why');
+});
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
