2811284083c03fb86c8530e198c582924e1f4df8
VishalCoder78 <vishalcoder78@gmail.com>
2023-06-14 16:16:09 +0530

    create admission migration done

--- app/Http/Controllers/admissionController.php
+++ app/Http/Controllers/admissionController.php
@@ -11,14 +11,14 @@ class admissionController extends Controller
     function addmission (Request $req)
     {
         $admission = new Admission;
-        $admission->f_name = $req->f_name;
-        $admission->l_name = $req->l_name;
+        $admission->first_name = $req->f_name;
+        $admission->last_name = $req->l_name;
         $admission->father_name = $req->father;
         $admission->mother_name = $req->mother;
         $admission->mobile_no = $req->mobile;
         $admission->email = $req->email;
-        $admission->birth = $req->dob;
-        $admission->addreas = $req->address;
+        $admission->birth_date = $req->dob;
+        $admission->address = $req->address;
         $admission->save();
         return redirect()->back()->with('success', 'Admission Successfull');
 
